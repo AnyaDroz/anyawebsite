1aed328f60c4226b0aa9ba456bc2d4efcb34844e
AnyaDroz <anya.drozdova89@gmail.com>
2024-07-31 08:45:11 +0100

    feat: update about

--- src/homepage/About.tsx
+++ src/homepage/About.tsx
@@ -9,14 +9,11 @@ export default function About() {
       <Grid className={clsx(styles.container)}>
         <div className={styles.about}>
           <p>
-            Hello - I’m Anya, an experienced product designer and creative
-            front-end developer who designs with code in mind. I enjoy solving
-            problems with clean scalable solutions and creating elegant digital
-            products and brand experiences. I am a highly visual thinker working
-            with React, Angular, Storybook, Framer... to name a few. This blend
-            of user experience and development helps me create business value
-            both at the macro level of winning a users’ attention, to the micro
-            level of components and consistent, flexible design systems.
+            Hello - I’m Anya, an experienced software engineer and UI/UX
+            designer. I enjoy solving problems with clean scalable solutions and
+            creating elegant digital products and brand experiences. I am a
+            highly visual thinker working with Typescript, React, Angular,
+            Storybook, Framer... to name a few.
           </p>
           <p>Website built with React.</p>
         </div>
